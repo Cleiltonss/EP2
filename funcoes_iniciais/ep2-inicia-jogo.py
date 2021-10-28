c2d76897fbfc1f1be7d49463f5c0edb60abfc96a
Cleiltonss <cleilton.mecatronico@gmail.com>
2021-10-28 19:58:53 -0300

    inicia-jogo corrigido
    nova correção do código

---
+++ funcoes_iniciais/ep2-inicia-jogo.py
@@ -0,0 +1,8 @@
+def inicia_jogo(n_jog, lista_pecas):
+    dic_final = {'jogadores': {}}
+    for i in range(0, n_jog):        
+        dic_final['jogadores'][i] = lista_pecas[0:7]
+        del lista_pecas[0:7]
+    dic_final['monte'] = lista_pecas
+    dic_final['mesa'] = []
+    return dic_final

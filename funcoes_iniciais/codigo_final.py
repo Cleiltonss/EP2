b05a96efa45c3428748458f95b1ad0c848dfe904
talesitf <talesitf@al.insper.edu.br>
2021-11-01 06:43:33 -0300

    Revisar os comentários e formatar para A+

--- funcoes_iniciais/codigo_final.py
+++ funcoes_iniciais/codigo_final.py
@@ -23,49 +23,86 @@ while inicia != 'não':
 
     #Início do Jogo
     dic_jmm = inicia_jogo(n_jogadores, lista_pecas) #dicionario com jogador, monte e peças
-    jogador_inicial = randint(0, n_jogadores - 1)
-    jogador = jogador_inicial
-    jogo_fim = False
-    joga_peca = 0
-    mao = dic_jmm['jogadores']#retorna um dic de keys '0' a '3'
-    mesa = dic_jmm['mesa']#retorna uma lista cheia ou vazia
-    monte = dic_jmm['monte']#retorna uma lista vazia
-    while jogo_fim != True:
+    jogador_inicial = randint(0, n_jogadores - 1) #define o indice do jogador inicial
+    jogador = jogador_inicial #jogador é uma variável que vai representar quem está jogando na rodada
+    jogo_fim = False #define que o jogo está rodando
+    joga_peca = 0 #cria a variavel da peça a ser jogada na mesa em cada rodada
+    pontos = [] #um dicionário com os pontos de cada jogador para classificação e caso dê empate
+    mao = dic_jmm['jogadores'] #retorna um dic com as mãos do jogadores da partida
+    mesa = dic_jmm['mesa'] #retorna uma lista vazia de inicio
+    monte = dic_jmm['monte']#retorna uma lista com o monte de inicio da partida
+
+
+    while jogo_fim != True: #Se o jogo n tiver acabado
+        
+        #Vemos a mesa e quem está jogando primeiro
+        print('MESA: \n{}'.format(mesa))
         if jogador == 0:
             print('Jogador: Você com {} peça(s)'.format(len(mao[jogador])))
         else:
             print('Jogador: {} com {} peça(s)'.format((jogador+1),(len(mao[jogador]))))
-        peg_monte = True
-        while peg_monte:
-            pecas_possiveis = posicoes_possiveis(mesa,mao[jogador])
-            if pecas_possiveis == []:
-                if monte != []:
-                    print('Não tem peças possíveis. PEGANDO DO MONTE!')
-                    mao[jogador].append(monte[0])
-                    del monte[0]
+        
+        #Agora, cheacamos se o jogador tem peças que podem ser jogadas e qual vai ser adicionada na mesa
+        pecas_possiveis = posicoes_possiveis(mesa,mao[jogador])
+        
+        if pecas_possiveis == []:
+            if monte != []:                                          #jogador começa a puxar do monte
+                print('Não tem peças possíveis. PEGANDO DO MONTE!')
+                mao[jogador].append(monte[0])
+                del monte[0]
+            else:                                                    #o monte acabou, a contagem de empate incrementa
+                print('Não há peças no monte! PASSANDO A VEZ!')
+                jogador +=1
+                empate+=1
+            if empate==n_jogadores: #Quer dizer que todos os jogadores precisaram pegar do monte, mas ele está vazio
+                jogo_fim = True     #Então, a partida acaba
+                for j in mao.keys():
+                    pontos.append(soma_pecas(mao[j]))   #Uma tabela dos pontos de cada jogador
+                ganha = min(pontos)
+                vencedor = []
+                for j in mao.keys():
+                    if mao[j] == ganha:
+                        vencedor.append(j+1)
+                if len(vencedor)>1:
+                    print('Venceram os jogadores: {}'.format(vencedor))
                 else:
-                    print('Não há peças no monte! PASSANDO A VEZ!')
-                    peg_monte = False
-            else:
+                    print('Venceu o jogador: {}'.format(vencedor))
+                
+                
+
+                
+        
+        
+        else:                                                        #O jogador possui ao menos uma peça possivel de ser jogada    
+            empate = 0                                               #A contagem de empate deve ser resetada então
+            
+            if jogador == 0:                                         #As açoes do jogador ZERO são representadas pelo jogador    
+                
                 escolha = ''
                 for num in pecas_possiveis:
-                    escolha += ',{}'.format(num+1)
-                if jogador == 0:
-                    joga_peca = int(input('Escolha qual peca jogar: {}'.format(escolha))) -1
-                else:
-                    joga_peca = randint(0,len(pecas_possiveis)-1)
-                    
-                peg_monte = False
+                    escolha += ',{}'.format(num+1)                   #Informa ao jogador humano as peças que ele pode jogar
+                escolha = escolha[1:]
                 
-                mesa = adiciona_na_mesa(mao[jogador][joga_peca],mesa)
-                del mao[jogador][joga_peca]
-        vencedor = verifica_ganhador(mao)
-        if vencedor == -1:
-            jogador+=1        
-            if jogador==n_jogadores:
-                jogador =0
-        else:
+                joga_peca = int(input('Escolha qual peca jogar: {}'.format(escolha))) -1 #retorna o indice da peça que ele quer jogar
+            
+            
+            else:                                                   #O computador escolhe aleatoriamente uma das peças possiveis para jogar
+                joga_peca = randint(0,len(pecas_possiveis)-1)
+                    
+            mesa = adiciona_na_mesa(mao[jogador][joga_peca],mesa)   #A mesa agora recebe a peça que jogaram
+            print('Colocou: {}'.format(mao[jogador][joga_peca]))
+            del mao[jogador][joga_peca]                             #É informado a todos os jogadores qual peça foi jogada
+            
+            
+            jogador+=1                                              #Passa a vez ao próximo jogador
+            if jogador == n_jogadores:
+                jogador = 0                                         #Garante que cada rodada vai ser cíclica 
+        
+        vencedor = verifica_ganhador(mao)                           #Checa se há algum ganhador nessa rodada
+        if vencedor != -1:
             jogo_fim = True
-            print('Venceu o jogador: {}'.format(jogador))
+            print('Venceu o jogador: {}'.format(jogador+1))
+
+            
     inicia = input('Quer iniciar um jogo? (sim/não)')
 print('Foi um prazer jogar com vc!')                    
